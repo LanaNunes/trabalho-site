2393aa80581bed40d5eccda9a6d1929095bd6109
LanaNunes <alanasilvanunes08@gmail.com>
2025-01-30 19:22:05 -0300

    Add files via upload

---
+++ Lana-Nunys-enviar/script.js
@@ -0,0 +1,5 @@
+cons title=document.getElementbyid("title")
+const botaoMudanca= document.getElementbyid("botaoMudanca");
+changeContentButton. addEventListener("click",()=>{ 
+    title.innerhtml="adicionado ao carrinho";})
+)
